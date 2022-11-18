f786c238267d6221c845a6ed011e4682248f1e5b
Henrik Wilnersson <hw222nq@student.lnu.se>
2022-11-18 11:00:15 +0100

    Add working app flow

--- app/src/main/java/cryptologyApp/controller/MainController.java
+++ app/src/main/java/cryptologyApp/controller/MainController.java
@@ -1,5 +1,7 @@
 package cryptologyApp.controller;
 
+import java.time.LocalDateTime;
+
 import cryptologyApp.model.FileHandler;
 import cryptologyApp.model.KeyInterface;
 import cryptologyApp.model.SubstitutionAlgorithm;
@@ -10,6 +12,7 @@ import cryptologyApp.view.Menu.EncryptionMethod;
 
 public class MainController {
   private ConsoleView view;
+  private FileHandler fileHandler;
   private EncryptionMethod selectedEncryptionMethod;
   private CryptologyAction selectedAction;
   private KeyInterface selectedKey;
@@ -18,22 +21,15 @@ public class MainController {
 
   public MainController(ConsoleView view) {
     this.view = view;
+    this.fileHandler = new FileHandler();
   }
 
   public void start() {
-    this.view.displayEncryptionMethodSelection();
-    this.selectedEncryptionMethod = this.view.getEncryptionMethodSelection();
-
-    this.view.displayCryptologyActionSelection();
-    this.selectedAction = this.view.getCryptologyActionSelection();
-
-    this.view.displayKeyInputDialogue(selectedEncryptionMethod);
-    this.selectedKey = this.view.getUserKey(selectedEncryptionMethod);
-
-    FileHandler fileHandler = new FileHandler();
-
-    this.view.displaySourceFileNameInputDialogue();
-    this.input = fileHandler.readFromFile(this.view.getSourceFileName());
+    this.setEncryptionMethod();
+    this.setCryptologyAction();
+    this.setKey();
+    this.setInput();
+    this.runProgramCycle();
 
     // Debug
     System.out.println("You selected " + this.selectedEncryptionMethod + " and " + this.selectedAction + " with key: " + this.selectedKey.getKey());
@@ -44,7 +40,56 @@ public class MainController {
     String encryptedInput = algorithm.encrypt(this.input);
     System.out.println("Encrypted string:\n" + encryptedInput);
     System.out.println("Decrypted string:\n" + algorithm.decrypt(encryptedInput));
+  }
 
-    fileHandler.writeToFile(encryptedInput, "test-output.txt");
+  private void setEncryptionMethod() {
+    this.view.displayEncryptionMethodSelection();
+    this.selectedEncryptionMethod = this.view.getEncryptionMethodSelection();
+  }
+
+  private void setCryptologyAction() {
+    this.view.displayCryptologyActionSelection();
+    this.selectedAction = this.view.getCryptologyActionSelection();
+  }
+
+  private void setKey() {
+    this.view.displayKeyInputDialogue(selectedEncryptionMethod);
+    this.selectedKey = this.view.getUserKey(selectedEncryptionMethod);
+  }
+
+  private void setInput() {
+    FileHandler fileHandler = new FileHandler();
+
+    this.view.displaySourceFileNameInputDialogue();
+    this.input = fileHandler.readFromFile(this.view.getSourceFileName());
+  }
+
+  private void runProgramCycle() {
+    if (selectedEncryptionMethod == EncryptionMethod.SUBSTITUTION) {
+      this.runSubstitution();
+    } else if (selectedEncryptionMethod == EncryptionMethod.TRANSPOSITION) {
+      this.runTransposition();
+    }
+  }
+
+  private void runSubstitution() {
+    SubstitutionAlgorithm algorithm = new SubstitutionAlgorithm((SubstitutionKey)this.selectedKey);
+
+    if (selectedAction == CryptologyAction.ENCRYPT) {
+      String cipherText = algorithm.encrypt(this.input);
+      this.createOutputFile(cipherText, "substitution-encryption");
+    } else if (selectedAction == CryptologyAction.DECRYPT) {
+      String plainText = algorithm.decrypt(this.input);
+      this.createOutputFile(plainText, "substitution-decryption");
+    }
+  }
+
+  private void runTransposition() {
+    // TODO: Implement transposition cycle.
+  }
+
+  private void createOutputFile(String fileContent, String fileName) {
+    String currentDateTime = java.time.LocalDateTime.now().toString();
+    this.fileHandler.writeToFile(fileContent, fileName + "_" + currentDateTime + ".txt");
   }
 }
