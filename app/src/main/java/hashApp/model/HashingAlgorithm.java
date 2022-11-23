19a658ced02243e9cfc2c4300be47b795e35b48f
Henrik Wilnersson <hw222nq@student.lnu.se>
2022-11-23 15:33:10 +0100

    Add constant for nr of buckets

--- app/src/main/java/hashApp/model/HashingAlgorithm.java
+++ app/src/main/java/hashApp/model/HashingAlgorithm.java
@@ -1,15 +1,17 @@
 package hashApp.model;
 
 public class HashingAlgorithm {
+  private final int NR_OF_BUCKETS = 256;
+
   public int hash(String input) {
     long inputTotalValue = 0;
 
     for (int i = 0; i < input.length(); i++) {
       int currentCharValue  = input.charAt(i);
-      inputTotalValue += currentCharValue * currentCharValue;
+      inputTotalValue += Math.pow(currentCharValue, 2);
     }
 
-    int result = (int)(inputTotalValue % 256);
+    int result = (int)(inputTotalValue % NR_OF_BUCKETS);
 
     return result;
   }
