8eef53e8f7d8fb2971e267480121f60e6eebab65
Henrik Wilnersson <hw222nq@student.lnu.se>
2022-11-23 13:43:24 +0100

    Remove unnecessary newline in view

--- app/src/main/java/hashApp/view/ConsoleView.java
+++ app/src/main/java/hashApp/view/ConsoleView.java
@@ -12,7 +12,7 @@ public class ConsoleView {
   public void displayInputFileDialogue() {
     System.out.println("This application will take a txt-file and apply a hashing algorithm to its content.\n" +
       "Please note that each row in the file will be treated as a separate input string, and will generate it's own hash.");
-    System.out.println("Enter source filename (must be located in the 'app/data' folder): ");
+    System.out.print("Enter source filename (must be located in the 'app/data' folder): ");
   }
 
   public String getSourceFileName() {
