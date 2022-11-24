7ee866fea84651c4ae34a859d24b671c8fe05ea8
Henrik Wilnersson <hw222nq@student.lnu.se>
2022-11-24 15:08:16 +0100

    Remove magic constants from key classes

--- app/src/main/java/cryptologyApp/model/SubstitutionKey.java
+++ app/src/main/java/cryptologyApp/model/SubstitutionKey.java
@@ -3,6 +3,8 @@ package cryptologyApp.model;
 public class SubstitutionKey implements KeyInterface {
   private String key;
   private static final int CHAR_POSITION_ZERO = 0;
+  private static final int MIN_ALLOWED_CHAR_VALUE = 0;
+  private static final int MAX_ALLOWED_CHAR_VALUE = 255;
 
   public SubstitutionKey(String key) {
     this.validateKey(key);
@@ -14,7 +16,8 @@ public class SubstitutionKey implements KeyInterface {
       throw new IllegalArgumentException("Key must be exactly one character in length.");
     }
 
-    if (key.charAt(CHAR_POSITION_ZERO) < 0 || key.charAt(CHAR_POSITION_ZERO) > 255) {
+    if (key.charAt(CHAR_POSITION_ZERO) < MIN_ALLOWED_CHAR_VALUE
+        || key.charAt(CHAR_POSITION_ZERO) > MAX_ALLOWED_CHAR_VALUE) {
       throw new IllegalArgumentException("Key must be part of the ASCII- or Extended ASCII-table.");
     }
   }
--- app/src/main/java/cryptologyApp/model/TranspositionKey.java
+++ app/src/main/java/cryptologyApp/model/TranspositionKey.java
@@ -2,6 +2,8 @@ package cryptologyApp.model;
 
 public class TranspositionKey implements KeyInterface {
   private String key;
+  private static final int MIN_ALLOWED_CHAR_VALUE = 0;
+  private static final int MAX_ALLOWED_CHAR_VALUE = 255;
 
   public TranspositionKey(String key) {
     this.validateKey(key);
@@ -10,7 +12,7 @@ public class TranspositionKey implements KeyInterface {
 
   private void validateKey(String key) {
     for (int i = 0; i < key.length(); i++) {
-      if (key.charAt(i) < 0 || key.charAt(i) > 255) {
+      if (key.charAt(i) < MIN_ALLOWED_CHAR_VALUE || key.charAt(i) > MAX_ALLOWED_CHAR_VALUE) {
         throw new IllegalArgumentException("Key must only contain characters from the ASCII- or Extended ASCII-table.");
       }
     }
